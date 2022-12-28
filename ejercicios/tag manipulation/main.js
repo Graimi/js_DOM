c9e04b1bb4f1960bc219051d846862dd88f1a6bc
Graimi <113901525+Graimi@users.noreply.github.com>
2022-12-28 18:02:10 +0100

    first exercise done

--- ejercicios/tag manipulation/main.js
+++ ejercicios/tag manipulation/main.js
@@ -1,14 +1,33 @@
 // 1. Cambia el contenido del elemento h1 contenido en el div con id "title-container" para que contenga "The Power Title".
 // 🚨 No uses element.innerText, en su lugar usa element.innerHTML, ya que el engine de test no lo soporta. 🚨
-
+const title = document.querySelector('#title-container > h1')
+title.innerHTML = 'The Power Title'
 // Ahora añade un elemento "p" dentro del elemento con id "title-container" que tenga como texto "Agregando tags al DOM!"
+const divTitle = document.querySelector('#title-container')
+divTitle.innerHTML += '<p> Agregando tags al DOM! </p>'
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////
 // 2. Busca la lista con id "list" en el DOM y añade dinámicamente 5 elementos "li" en su interior.
 // Los elementos li deben tener la clase "li-element".
+const list = document.querySelector('ul#list');
+for (let i = 0; i < 5; i++) {
+  const liElement = document.createElement('li');
+  liElement.className = 'li-element';
+  list.append(liElement);
+}
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////
-// 3. Busca el botón con id "aggregator" y añádele un evento de forma que, cada vez que se clicke, un elemento
-// de tipo "span" será introducido en el elemento con id "span-container" de tipo "div" que ya existe en el HTML.
+// 3. Busca el botón con id "aggregator" y añádele un evento de forma que, cada vez que se clicke,
+// un elemento de tipo "span" será introducido en el elemento con id "span-container" de tipo
+// "div" que ya existe en el HTML.
 // Los span deben contener el texto "He sido creado con JS".
-// 🚨 No uses element.innerText, en su lugar usa element.innerHTML, ya que el engine de test no lo soporta. 🚨
+// 🚨 No uses element.innerText, en su lugar usa element.innerHTML, 
+// ya que el engine de test no lo soporta. 🚨
+const button = document.querySelector('button#aggregator')
+const spanContainer = document.querySelector('div#span-container')
+
+const handleAddSpan = () => {
+    spanContainer.innerHTML += '<span> He sido creado con JS </span>'
+  };
+
+  button.addEventListener('click', handleAddSpan)
